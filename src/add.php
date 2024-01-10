4a433804b91cb0bba9447277a9fb4f710dd6a778
ThanapolC <148734828+ThanapolC@users.noreply.github.com>
2024-01-10 14:49:10 +0900

    finish add.php

--- src/add.php
+++ src/add.php
@@ -4,11 +4,11 @@
         <table>
             <tr>
                 <th>出発日</th>
-                <td><input type="date" name="departure_date_input" required></td>
+                <td><input type="date" name="departure_input" required></td>
             </tr>
             <tr>
                 <th>帰着日</th>
-                <td><input type="date" name="return_date_input" required></td>
+                <td><input type="date" name="return_input" required></td>
             </tr>
             <tr>
                 <th>旅行先</th>
@@ -21,7 +21,7 @@
                         $pdo=new PDO($connect,USER,PASS);
 
                         $sql=$pdo->query('select * from Category');
-                        echo '<select name="category_id_input" required>';
+                        echo '<select name="category_input" required>';
                         foreach($sql as $row){
                             echo '<option value="',$row['category_id'],'">', $row['category_name'], '</option>';
                         }
--- src/index.php
+++ src/index.php
@@ -1,5 +1,12 @@
 <?php require 'db-connect.php'; ?>
 <?php require 'header.php'; ?>
+<?php $pdo=new PDO($connect, USER, PASS); ?>
+    <?php
+        if(isset($_POST['add'])){
+            $sql2=$pdo->prepare('insert into Journey value(null,?,?,?,?)');
+            $sql2->execute([$_POST['departure_input'],$_POST['return_input'],$_POST['destination_input'],$_POST['category_input'],]);
+        } 
+    ?>
     <table>
         <thead>
             <tr>
@@ -11,8 +18,6 @@
         </thead>
         <tbody>       
             <?php
-                $pdo=new PDO($connect, USER, PASS);
-
                 $sql=$pdo->query('select departure_date, return_date, destination, category_name from Journey inner join Category on Journey.category_id = Category.category_id order by departure_date');
                 foreach ($sql as $row){
                     echo '<tr>';
