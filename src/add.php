413050cadf79b42a5123a4689c4ffd46e8f534c1
ThanapolC <148734828+ThanapolC@users.noreply.github.com>
2024-01-16 13:33:38 +0900

    finish

--- src/add.php
+++ src/add.php
@@ -2,33 +2,53 @@
 <?php require 'header.php'; ?>
 <?php $pdo=new PDO($connect, USER, PASS); ?>
     <form action="index.php" method="post">
-        <table>
-            <tr>
-                <th>出発日</th>
-                <td><input type="date" name="departure_input" required></td>
-            </tr>
-            <tr>
-                <th>帰着日</th>
-                <td><input type="date" name="return_input" required></td>
-            </tr>
-            <tr>
-                <th>旅行先</th>
-                <td><input type="text" name="destination_input" required></td>
-            </tr>
-            <tr>
-                <th>カテゴリー</th>
-                <td>
-                    <?php
-                        $sql=$pdo->query('select * from Category');
-                        echo '<select name="category_input" required>';
-                        foreach($sql as $row){
-                            echo '<option value="',$row['category_id'],'">', $row['category_name'], '</option>';
-                        }
-                        echo '</select>';
-                    ?>
-                </td>
-            </tr>
-        </table>
-        <input type="submit" value="登録" name="add">
+        <div class="columns is-centered has-text-left">
+            <table class="table">
+                <tr>
+                    <th class="thead is-size-5 has-text-weight-semibold">出発日</th>
+                    <td><input class="input is-small" type="date" name="departure_input" required></td>
+                </tr>
+                <tr>
+                    <th class="thead is-size-5 has-text-weight-semibold">帰着日</th>
+                    <td><input class="input is-small" type="date" name="return_input" required></td>
+                </tr>
+                <tr>
+                    <th class="thead is-size-5 has-text-weight-semibold">旅行先</th>
+                    <td>
+                        <div class="field">
+                            <p class="control has-icons-left">
+                                <input class="input is-small" type="text" name="destination_input" required>
+                                <span class="icon is-small is-left">
+                                    <i class="fas fa-city"></i>
+                                </span>
+                            </p>
+                        </div>   
+                    </td>
+                </tr>
+                <tr>
+                    <th class="thead is-size-5 has-text-weight-semibold">カテゴリー</th>
+                    <td>
+                        <div class="control has-icons-left">
+                            <div class="select is-small">
+                                <?php
+                                    $sql=$pdo->query('select * from Category');
+                                    echo '<select name="category_input" required>';
+                                    foreach($sql as $row){
+                                        echo '<option value="',$row['category_id'],'">', $row['category_name'], '</option>';
+                                    }
+                                    echo '</select>';
+                                ?>
+                            </div>
+                            <span class="icon is-left">
+                                <i class="fas fa-globe"></i>
+                            </span>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class="btn has-text-centered">
+            <input class="button is-warning is-normal is-rounded" type="submit" value="登録" name="add">
+        </div>    
     </form>
 <?php require 'footer.php'; ?>
--- src/edit_confirm.php
+++ src/edit_confirm.php
@@ -11,37 +11,57 @@
     $data = $sql2->fetch();    
 ?>
     <form action="index.php" method="post">
-        <table>
-            <tr>
-                <th>出発日</th>
-                <td><input type="date" name="departure_input" required value="<?= $data['departure_date'] ?>"></td>
-            </tr>
-            <tr>
-                <th>帰着日</th>
-                <td><input type="date" name="return_input" required value="<?= $data['return_date'] ?>"></td>
-            </tr>
-            <tr>
-                <th>旅行先</th>
-                <td><input type="text" name="destination_input" required value="<?= $data['destination'] ?>"></td>
-            </tr>
-            <tr>
-                <th>カテゴリー</th>
-                <td>
-                    <?php
-                        echo '<select name="category_input" required>';
-                        foreach($sql as $row){
-                            $select = "";
-                            if($data['category_id'] == $row['category_id']){
-                                $select = "selected";
-                            }
-                            echo '<option value="', $row['category_id'], '"', $select, '>', $row['category_name'], '</option>';
-                        }
-                        echo '</select>';
-                    ?>
-                </td>
-            </tr>
-        </table>
+        <div class="columns is-centered has-text-left">
+            <table class="table">
+                <tr>
+                <th class="thead is-size-5 has-text-weight-semibold">出発日</th>
+                    <td><input class="input is-small" type="date" name="departure_input" required value="<?= $data['departure_date'] ?>"></td>
+                </tr>
+                <tr>
+                    <th class="thead is-size-5 has-text-weight-semibold">帰着日</th>
+                    <td><input class="input is-small" type="date" name="return_input" required value="<?= $data['return_date'] ?>"></td>
+                </tr>
+                <tr>
+                    <th class="thead is-size-5 has-text-weight-semibold">旅行先</th>
+                    <td>
+                        <div class="field">
+                            <p class="control has-icons-left">
+                                <input class="input is-small" type="text" name="destination_input" required value="<?= $data['destination'] ?>">
+                                <span class="icon is-small is-left">
+                                    <i class="fas fa-city"></i>
+                                </span>
+                            </p>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <th class="thead is-size-5 has-text-weight-semibold">カテゴリー</th>
+                    <td>
+                        <div class="control has-icons-left">
+                            <div class="select is-small">
+                                <?php
+                                    echo '<select name="category_input" required>';
+                                    foreach($sql as $row){
+                                        $select = "";
+                                        if($data['category_id'] == $row['category_id']){
+                                            $select = "selected";
+                                        }
+                                        echo '<option value="', $row['category_id'], '"', $select, '>', $row['category_name'], '</option>';
+                                    }
+                                    echo '</select>';
+                                ?>
+                            </div>
+                            <span class="icon is-left">
+                                <i class="fas fa-globe"></i>
+                            </span>
+                        </div>    
+                    </td>
+                </tr>
+            </table>
+        </div>    
         <input type="hidden" name="id_confirm" value="<?= $id ?>">
-        <input type="submit" value="更新" name="edit">
+        <div class="btn has-text-centered">
+            <input class="button is-warning is-normal is-rounded" type="submit" value="更新" name="edit">
+        </div>    
     </form>
 <?php require 'footer.php'; ?>
